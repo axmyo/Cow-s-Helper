7b3c4844679937c5c087c13a87d14fb356487715
axmyo <86020386+axmyo@users.noreply.github.com>
2021-10-31 18:49:20 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -13,7 +13,7 @@ module.exports.run = async (client, message, args) => {
 	if (check) return;
 	if(target.id === message.author.id) return message.reply('Why would you want to ban yourself?!', { allowedMentions: { repliedUser: false } });
 	if(target.id === client.user.id) return message.reply('Why would you want to ban me?!', { allowedMentions: { repliedUser: false } });
-        if(target.roles.cache.has("742803994816020503") return message.reply("That's a mod, no.")), 
+        if(target.roles.cache.has("742803994816020503")) return message.reply("That's a mod, no."), 
     let reason = "No reason provided.";
     if (args[1]) reason = args.splice(1).join(" ");
 	if (!target.bannable) return message.reply('Can\'t ban specified member! Make sure I\'m above them in the heirarchy', { allowedMentions: { repliedUser: false } });
