3d8e805226b1184b656a7376351c9d37e0838cb9
axmyo <86020386+axmyo@users.noreply.github.com>
2021-10-31 19:01:50 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -7,7 +7,8 @@ const { Client, Message, MessageEmbed } = require('discord.js');
  * @param {String} args
  */
 module.exports.run = async (client, message, args) => {
-	if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You are not allowed to unban members!');
+	let logs = await client.channels.fetch('868645138770051152')
+if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You are not allowed to unban members!');
 	if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send('I am not allowed to unban members!');
 	if (!args[0]) return message.reply('Please provide a user id to unban!', { allowedMentions: { repliedUser: false } });
 	const toUnBan = client.users.cache.get(args[0].match(/[1234567890]{18}/igm)[0]) || await client.users.fetch(args[0], true, true);
@@ -28,7 +29,7 @@ module.exports.run = async (client, message, args) => {
          .addField("**Unbanned By**", message.author.username)
          .addField("**Date**", message.createdAt.toLocaleString())
          .setTimestamp();
-	message.channel.send(unbanEmbed)
+	logs.send(unbanEmbed)
 };
 module.exports.config = {
     name: "unban",
