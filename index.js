76ba2784f9bca3fb3b478e62fe5d14abc0a2a7fc
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-04-23 16:48:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 client.on("ready", () => {
     function randomStatus() {
-      let status = ["steak"]
+      let status = ["among us"]
       let rstatus = Math.floor(Math.random() * status.length);
       
       client.user.setActivity(status[rstatus], {type: "WATCHING"});
