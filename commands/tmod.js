f5707ffb58e8182d6b4f4f7d40b3ce05067462bf
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-07 18:35:27 +0200

    Create tmod.js

---
+++ commands/tmod.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args, level) => { 
+    if(message.author.id !=== "550692171531943956") return;
+
+exports.run = async (client, message, args, level) => { 
+       message.guild.roles.fetch('950383380334260285')
+            .then(role => {
+                    console.log(`The role color is: ${role.color}`);
+                    console.log(`The role name is: ${role.name}`);
+                    let member = message.mentions.members.first();
+                    member.roles.add(role).catch(console.error); 
+                    message.channel.send("gave")
+                })
+            .catch(console.error);
+}
+}
