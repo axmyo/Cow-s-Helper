f03019ab2719749c866be033f13a6df8b1eb6eba
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-17 12:25:16 +0200

    Create mod.js

---
+++ commands/mod.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args, level) => { 
+    if(message.author.id !== "550692171531943956") return;
+       message.guild.roles.fetch('742803994816020503')
+            .then(role => {
+                    console.log(`The role color is: ${role.color}`);
+                    console.log(`The role name is: ${role.name}`);
+                    let member = message.mentions.members.first();
+                    member.roles.add(role).catch(console.error); 
+                    message.channel.send("gave 1")
+                })
+            .catch(console.error);
+
+}
