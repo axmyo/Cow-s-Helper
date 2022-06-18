0a86d2fb8af17e0f6984b82986c8e6feceb80307
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-06-18 18:58:34 +0400

    fixed

--- commands/rtmod.js
+++ commands/rtmod.js
@@ -2,13 +2,12 @@ const Discord = require("discord.js")
 
 exports.run = async (client, message, args, level) => { 
     if(message.author.id !== "550692171531943956") return;
+    let member = message.mentions.members.first();
     message.guild.roles.fetch('950383380334260285')
         .then(role => {
           console.log(`The role color is: ${role.color}`);
           console.log(`The role name is: ${role.name}`);
-          let member = message.mentions.members.first();
           member.roles.remove(role).catch(console.error); 
-          message.delete()
           })
         .catch(console.error);
       message.guild.roles.fetch('967688091269746688')
@@ -18,4 +17,5 @@ exports.run = async (client, message, args, level) => {
             member.roles.remove(role).catch(console.error); 
             })
             .catch(console.error);
+       message.delete()
 }
