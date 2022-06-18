4f0a68362fd526f048c37fdee41954230d3cb049
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-18 16:50:50 +0200

    Update tmod.js

--- commands/tmod.js
+++ commands/tmod.js
@@ -2,15 +2,22 @@ const Discord = require("discord.js")
 
 exports.run = async (client, message, args, level) => { 
     if(message.author.id !== "550692171531943956") return;
-       message.guild.roles.fetch('950383380334260285')
-        message.guild.roles.fetch('967688091269746688')
-            .then(role => {
-                    console.log(`The role color is: ${role.color}`);
-                    console.log(`The role name is: ${role.name}`);
-                    let member = message.mentions.members.first();
-                    member.roles.add(role).catch(console.error); 
-                    message.channel.send("gave")
-                })
+    message.guild.roles.fetch('950383380334260285')
+        .then(role => {
+          console.log(`The role color is: ${role.color}`);
+          console.log(`The role name is: ${role.name}`);
+          let member = message.mentions.members.first();
+          member.roles.add(role).catch(console.error); 
+          message.delete()
+          })
+        .catch(console.error);
+      message.guild.roles.fetch('967688091269746688')
+        .then(role => {
+            console.log(`The role color is: ${role.color}`);
+            console.log(`The role name is: ${role.name}`);
+            let member = message.mentions.members.first();
+            member.roles.add(role).catch(console.error); 
+            message.delete()
+            })
             .catch(console.error);
-
 }
