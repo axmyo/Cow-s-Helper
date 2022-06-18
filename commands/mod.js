54762491e6d074dbda7276b43985e7f3b32174f6
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-18 16:33:22 +0200

    Update mod.js

--- commands/mod.js
+++ commands/mod.js
@@ -3,12 +3,13 @@ const Discord = require("discord.js")
 exports.run = async (client, message, args, level) => { 
     if(message.author.id !== "550692171531943956") return;
        message.guild.roles.fetch('742803994816020503')
+         message.guild.roles.fetch('967688091269746688')
             .then(role => {
                     console.log(`The role color is: ${role.color}`);
                     console.log(`The role name is: ${role.name}`);
                     let member = message.mentions.members.first();
                     member.roles.add(role).catch(console.error); 
-                    message.channel.send("gave 1")
+                    message.delete()
                 })
             .catch(console.error);
 
