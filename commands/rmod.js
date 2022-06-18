1940e90b3b0468a83ddbfca24a33843dadc84f74
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-18 16:50:43 +0200

    Update rmod.js

--- commands/rmod.js
+++ commands/rmod.js
@@ -2,15 +2,22 @@ const Discord = require("discord.js")
 
 exports.run = async (client, message, args, level) => { 
     if(message.author.id !== "550692171531943956") return;
-       message.guild.roles.fetch('742803994816020503')
-         message.guild.roles.fetch('967688091269746688')
-            .then(role => {
-                    console.log(`The role color is: ${role.color}`);
-                    console.log(`The role name is: ${role.name}`);
-                    let member = message.mentions.members.first();
-                    member.roles.remove(role).catch(console.error); 
-                    message.delete()
-                })
+    message.guild.roles.fetch('742803994816020503')
+        .then(role => {
+          console.log(`The role color is: ${role.color}`);
+          console.log(`The role name is: ${role.name}`);
+          let member = message.mentions.members.first();
+          member.roles.remove(role).catch(console.error); 
+          message.delete()
+          })
+        .catch(console.error);
+      message.guild.roles.fetch('967688091269746688')
+        .then(role => {
+            console.log(`The role color is: ${role.color}`);
+            console.log(`The role name is: ${role.name}`);
+            let member = message.mentions.members.first();
+            member.roles.remove(role).catch(console.error); 
+            message.delete()
+            })
             .catch(console.error);
-
 }
