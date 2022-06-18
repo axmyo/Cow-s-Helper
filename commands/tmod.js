c67f8bf1eb85a8508a9f5ef4d3fdc09c739144bf
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-18 16:55:49 +0200

    Update tmod.js

--- commands/tmod.js
+++ commands/tmod.js
@@ -15,7 +15,6 @@ exports.run = async (client, message, args, level) => {
         .then(role => {
             console.log(`The role color is: ${role.color}`);
             console.log(`The role name is: ${role.name}`);
-            let member = message.mentions.members.first();
             member.roles.add(role).catch(console.error); 
             })
             .catch(console.error);
