5097f7b1eafe1b07e0fdf47f8d0fb158d9c88317
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-19 08:50:44 +0200

    Update rmod.js

--- commands/rmod.js
+++ commands/rmod.js
@@ -5,17 +5,18 @@ exports.run = async (client, message, args, level) => {
         let member = message.mentions.members.first();
     message.guild.roles.fetch('742803994816020503')
         .then(role => {
-          console.log(`The role color is: ${role.color}`);
-          console.log(`The role name is: ${role.name}`);
           member.roles.remove(role).catch(console.error); 
           message.delete()
           })
         .catch(console.error);
       message.guild.roles.fetch('967688091269746688')
         .then(role => {
-            console.log(`The role color is: ${role.color}`);
-            console.log(`The role name is: ${role.name}`);
             member.roles.remove(role).catch(console.error); 
+            })
+            .catch(console.error);
+     message.guild.roles.fetch('987972120350953476')
+        .then(role => {
+            member.roles.add(role).catch(console.error); 
             })
             .catch(console.error);
 }
