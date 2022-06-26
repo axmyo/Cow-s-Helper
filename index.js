2c9e339eb91999a8da8731338ebe60fcfc2bbeee
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-26 21:47:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -146,18 +146,5 @@ client.on("message", message => {
 }
 )
 
-client.on('messageCreate', (message) => {
-  if (message.content.startsWith("<slowmode")) {
-    if(message.member.permissions.has('MANAGE_MESSAGES') && message.mentions.channels.first()) {
-      var args = message.content.split(" ")
-      var channel = message.mentions.channels.first()
-      var time = args[1]
-      channel.setRateLimitPerUser(time)
-    }
-    else {
-      message.channel.send("no")
-    }
-  } 
-})
 
 client.login(config.token)
