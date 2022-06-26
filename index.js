f68b38e88078b457d6a385545481ff2d9f15d746
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-06-26 20:38:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ client.on("guildMemberUpdate", (oldMember, newMember) =>{
   }
 
   if(newMember.id === "943587026370973746"){
-    newMember.setNickname("emoball")
+    newMember.setNickname("emobell")
   }
 })
 
