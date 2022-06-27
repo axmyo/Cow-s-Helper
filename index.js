ac23dd363fedaf9fa88b8d274298c8eda3156f11
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-06-27 21:04:55 +0400

    fixed messageCreate to message

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.on("ready", () => {
     client.channels.cache.get("864780780109692969").send({files: [attachment]});
   }
 
-  client.on("messageCreate", async message => {
+  client.on("message", async message => {
     
     if((message.author.id == "159985870458322944") && (message.channel.id == "977785008125251634") && (message.mentions) && (message.content.includes("level"))){
     
