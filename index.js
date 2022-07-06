eabcb64d39da848079c639af150d6b580b6331ce
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-07-06 10:07:11 +0200

    remove fetching config

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const discordTranscripts = require('discord-html-transcripts');
-const config = require("./config.json");
 
 const client = new Discord.Client({
   disableEveryone: true
