9da05af24a21a22d586aab280d1cea3875dd82c3
Axmyo Dev <86020386+axmyo@users.noreply.github.com>
2022-07-06 08:59:51 +0200

    Create ruwudaddy.js

---
+++ commands/ruwudaddy.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args, level) => { 
+    if(message.author.id !== "550692171531943956") return;
+        let member = message.mentions.members.first();
+    message.guild.roles.fetch('927269272617619537')
+        .then(role => {
+          member.roles.remove(role).catch(console.error); 
+          message.delete()
+          })
+        .catch(console.error);
+}
